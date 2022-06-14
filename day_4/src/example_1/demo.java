c26ca9b6678cd3fef45b39c0877836afa5317801
Shashank <Shashank@DESKTOP-P8GLJUD>
2022-06-14 18:31:08 +0530

    first commit

---
+++ day_4/src/example_1/demo.java
@@ -0,0 +1,14 @@
+package example_1;
+
+public class demo {
+	int x;
+}
+class testdemo{
+	public static void main(String[] args) {
+		demo myobj = new demo();
+		myobj.x = 60;
+		System.out.println(myobj.x);
+
+	}
+
+}
---
+++ day_4/src/example_2/demo.java
@@ -0,0 +1,19 @@
+package example_2;
+
+public class demo {
+	int x;
+}
+class Testdemo{
+
+	public static void main(String[] args) {
+		demo myobj1 = new demo();
+		demo myobj2 = new demo();
+		myobj1.x = 20;
+		myobj2.x = 70;
+		System.out.println(myobj1.x);
+		System.out.println(myobj2.x);
+		
+
+	}
+
+}
